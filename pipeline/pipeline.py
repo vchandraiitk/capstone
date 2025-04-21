0f82b0fe7e88b8a0a780bbdd6fdb05514ecf2835
vchandraiitk <69245104+vchandraiitk@users.noreply.github.com>
2025-04-21 23:20:17 +0530

    Update pipeline.py

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -6,10 +6,10 @@ from pipeline.deploy_model import deploy_model
 PROJECT = "planar-sun-456513-i8"
 BUCKET = "capstone-group15"
 LOCATION = "us-central1"
-## do something 
+GCS_SUBDIR = "models/simple-v1/"
 
 if __name__ == "__main__":
     save_model()
     create_mar()
-    upload_mar(bucket_name=BUCKET)
-    deploy_model(PROJECT, LOCATION, BUCKET)
+    upload_mar(bucket_name=BUCKET, gcs_subdir=GCS_SUBDIR)
+    deploy_model(PROJECT, LOCATION, BUCKET, gcs_subdir=GCS_SUBDIR)

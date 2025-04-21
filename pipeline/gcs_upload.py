bb6da2451451b3cfef0978dab959325424a69614
vchandraiitk <69245104+vchandraiitk@users.noreply.github.com>
2025-04-21 23:17:23 +0530

    Update gcs_upload.py

--- pipeline/gcs_upload.py
+++ pipeline/gcs_upload.py
@@ -3,6 +3,10 @@ from google.cloud import storage
 def upload_mar(bucket_name, gcs_path="models/simple-v1/simple-v1.mar"):
     client = storage.Client()
     bucket = client.bucket(bucket_name)
-    blob = bucket.blob(gcs_path)
+    ##blob = bucket.blob(gcs_path)
+    ##blob.upload_from_filename("model-store/simple-v1.mar")
+    
+    blob = bucket.blob("models/simple-v1/simple-v1.mar")
     blob.upload_from_filename("model-store/simple-v1.mar")
     print(f"✅ Uploaded to gs://{bucket_name}/{gcs_path}")
+

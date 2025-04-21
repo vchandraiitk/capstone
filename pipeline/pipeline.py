b7987121309358298f98041ea3ced81e872993ee
vchandraiitk <69245104+vchandraiitk@users.noreply.github.com>
2025-04-21 10:09:22 +0530

    Update pipeline.py

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,13 +1,11 @@
-# pipeline/pipeline.py
-
 from google.cloud import aiplatform
 
 def run_pipeline():
-    aiplatform.init(project="planar-sun-456513-i8", location="us-central1")
+    aiplatform.init(project="planar-sun-456513", location="us-central1")
 
     model = aiplatform.Model.upload(
         display_name="simple-model",
-        artifact_uri="gs://capstone-group15/models/simple_model.pt",
+        artifact_uri="gs://capstone-group15/models/simple-v1/",  # ✅ folder only
         serving_container_image_uri="us-docker.pkg.dev/vertex-ai/prediction/pytorch-cpu.1-13:latest"
     )
 

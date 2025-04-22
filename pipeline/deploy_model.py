ad069ec01912da01629796dbe3edb01521eb3b99
vchandraiitk <69245104+vchandraiitk@users.noreply.github.com>
2025-04-22 09:06:52 +0530

    Update deploy_model.py

--- pipeline/deploy_model.py
+++ pipeline/deploy_model.py
@@ -1,11 +1,11 @@
 from google.cloud import aiplatform
 
-def deploy_model(project, location, bucket_name, gcs_subdir="models/simple-v1/"):
+def deploy_model(project, location, bucket_name, gcs_subdir="models/"):
     aiplatform.init(project=project, location=location)
 
     model = aiplatform.Model.upload(
         display_name="simple-v1",
-        artifact_uri=f"gs://{bucket_name}/{gcs_subdir}",  # must contain model.pt
+        artifact_uri=f"gs://{bucket_name}/{gcs_subdir}",  # ✅ will be gs://capstone-group15/models/
         serving_container_image_uri="us-docker.pkg.dev/vertex-ai/prediction/pytorch-gpu.1-13:latest",
     )
 
